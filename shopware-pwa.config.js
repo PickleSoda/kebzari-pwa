b1562549ac8386962a25219ad512b61c83bab9f5
AchiUgulava <achiugulava@gmail.com>
2023-05-25 00:01:25 +0400

    removed default

--- shopware-pwa.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  shopwareEndpoint: "https://kebzari.ge",
-  shopwareAccessToken: "SWSCZJKYWU4ZY3BKYWZXQKNKSW",
-  shopwareDomainsAllowList: ["https://kebzari.ge/ge","https://kebzari.ge" ],
-  // theme: "kebzari-theme",
-};
