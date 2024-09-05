a063ab3fbbcaf5ad93b6f02bb76694e05861b67c
AchiUgulava <achiugulava@gmail.com>
2024-09-05 04:04:21 +0400

    added nuxt and shopware configs

---
+++ shopware-pwa.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  shopwareEndpoint: "https://shopware.kebzari.ge/",
+  shopwareAccessToken: "SWSCZJKYWU4ZY3BKYWZXQKNKSW",
+  shopwareDomainsAllowList: ["https://kebzari.ge/ge","https://kebzari.ge/en", "https://kebzari.vercel.app/ge", "https://kebzari.vercel.app/en"],
+  // theme: "kebzari-theme",
+};
+
